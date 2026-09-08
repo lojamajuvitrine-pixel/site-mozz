24a095573d46b44be04a408f85dc441c2468813e
lojamajuvitrine-pixel <loja.majuvitrine@gmail.com>
2026-09-08 16:29:20 -0300

    Implement redirect for masculino1 to produtos
    Added a redirect from '/masculino1' to '/produtos'.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {
       { protocol: "https", hostname: "**.mitiendanube.com" },
       { protocol: "https", hostname: "**.bling.com.br" }
     ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/masculino1',
+        destination: '/produtos',
+        permanent: true,
+      },
+    ];
   }
 };
+
 export default nextConfig;
