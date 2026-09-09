04f34736b383b79c17f78fc86f9f347520355a63
lojamajuvitrine-pixel <loja.majuvitrine@gmail.com>
2026-09-09 09:23:46 -0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      allow: ["/", "/api/google-feed", "/api/meta-feed"],
       disallow: ["/api/", "/carrinho", "/checkout/"]
     },
     sitemap: `${siteUrl}/sitemap.xml`
